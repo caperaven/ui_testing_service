6ac15a14f6b13ad95401651cffb73cdb01b80c3b
caperaven <caperaven@gmail.com>
2023-10-04 16:12:24 +0200

    add task queue

--- main.py
+++ main.py
@@ -7,16 +7,16 @@
 # 403 Forbidden for authorization errors.
 # 404 Not Found for resource not found errors.
 # 500 Internal Server Error for server errors.
-
+import asyncio
 # 1. run this on my machine record a test and run it locally
 # 2. run this on my machine record a test and run it on a remote machine - hosted on a server
 # 3. daily execution of entire test suite remotely
 #
 
 import sys
+import threading
 from fastapi import FastAPI, Body, Query, HTTPException
 from typing import Dict, Optional
-from enum import Enum
 from uvicorn import run
 from src.globals import JsonType
 from process_api import process_api
@@ -24,18 +24,22 @@ from process_api.modules.selenium import SeleniumModule
 from process_api.modules.selenium.conversions import clean_google_recording, GoogleRecording
 from src.json_identifier import identify_json
 from src.test_runner import TestRunner
+from src.task_queue import TaskQueue
 
 app = FastAPI()
+queue = TaskQueue()
 
 SeleniumModule.register(process_api)
 process_api.logger.set_level("error")
+
 
 @app.get("/")
 async def index():
     return {"message": "Hello, world!"}
 
+
 @app.post("/convert_recording")
-def convert_recording(recording_json: Dict = Body(...)):
+async def convert_recording(recording_json: Dict = Body(...)):
     json_type = identify_json(recording_json)
 
     if json_type == JsonType.GOOGLE_RECORDING:
@@ -52,7 +56,31 @@ def convert_recording(recording_json: Dict = Body(...)):
 @app.post("/test")
 async def test(data: Dict = Body(...), browser: Optional[str] = Query("chrome")):
     json_type = identify_json(data)
-    return await TestRunner.test(process_api, data, browser, json_type)
+
+    job_id = await queue.add(TestRunner.test, process_api, data, browser, json_type)
+    threading.Thread(target=run_in_new_loop).start()
+    return {"job_id": job_id}
+
+
+@app.get("/test_status")
+async def test_status(job_id: str):
+    if job_id not in queue.statuses:
+        raise HTTPException(status_code=404, detail="Job not found")
+
+    status = queue.statuses[job_id]
+    return status
+
+
+def run_in_new_loop():
+    # Create a new event loop
+    loop = asyncio.new_event_loop()
+    # Set the loop as the default for this context
+    asyncio.set_event_loop(loop)
+    # Run the async function in the loop
+    loop.run_until_complete(queue.run_first_task())
+    # Close the loop after the task is done
+    loop.close()
+
 
 host_address = "127.0.0.1"
 host_port = 8000
---
+++ src/task_queue.py
@@ -0,0 +1,68 @@
+# this is a class that allows you to create a queue of tasks that can be executed in a separate thread
+import uuid
+import asyncio
+from datetime import datetime
+
+
+class TaskQueue:
+    def __init__(self):
+        self.queue = []
+        self.statuses = {}
+
+    async def add(self, fn, *args, **kwargs):
+        task_id = str(uuid.uuid4())
+
+        self.queue.append({
+            "id": task_id,
+            "fn": fn,
+            "args": args,
+            "kwargs": kwargs
+        })
+
+        self.statuses[task_id] = {
+            "status": "queued",
+            "start_time": None,
+            "end_time": None,
+            "duration": None,
+        }
+
+        return task_id
+
+    async def run_first_task(self):
+        if len(self.queue) == 0:
+            return
+
+        task = self.queue.pop(0)
+        task_id = task["id"]
+
+        fn = task["fn"]
+        args = task["args"]
+        kwargs = task["kwargs"]
+
+        status = self.statuses[task_id]
+        status["status"] = "running"
+        status["start_time"] = datetime.now()
+
+        try:
+            await fn(*args, **kwargs)
+        finally:
+            status = self.statuses[task_id]
+            status["status"] = "complete"
+            status["end_time"] = datetime.now()
+            status["duration"] = get_time_format(status["start_time"], status["end_time"])
+
+            # call self recursively to run the next task
+            await self.run_first_task()
+
+    async def remove(self, task_id):
+        del self.statuses[task_id]
+
+
+def get_time_format(start_time, end_time):
+    duration = end_time - start_time
+    hours, remainder = divmod(duration.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    milliseconds = duration.microseconds // 1000  # Convert microseconds to milliseconds
+
+    formatted_duration = "{:02}:{:02}:{:02}:{:03}".format(hours, minutes, seconds, milliseconds)
+    return formatted_duration
