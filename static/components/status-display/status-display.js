38c45e4f57cc0baa4494b47416ff980a77c409f5
caperaven <caperaven@gmail.com>
2023-10-11 12:34:53 +0200

    add UI to services

--- static/components/status-display/status-display.js
+++ static/components/status-display/status-display.js
@@ -7,15 +7,39 @@ export default class StatusDisplay extends crs.classes.BindableElement {
         return import.meta.url.replace(".js", ".html");
     }
 
-    async connectedCallback() {
-        super.connectedCallback();
+    async load() {
+        const template = this.shadowRoot.querySelector("template");
+        await crs.binding.inflation.manager.register("statuses", template);
+        await this.getStatus();
+    }
+
+    async disconnectedCallback() {
+        await crs.binding.inflation.manager.unregister("statuses");
+        await super.disconnectedCallback();
     }
 
     async getStatus() {
         const response = await fetch("/status");
-        const status = await response.json();
-        console.log(status)
+        const statuses = statusToArray(await response.json());
+
+        const elements = await crs.binding.inflation.manager.get("statuses", statuses, this.collection.children);
+        this.collection.innerHTML = "";
+        this.collection.append(...elements);
     }
+}
+
+function statusToArray(statusObject) {
+    const result = [];
+
+    const keys = Object.keys(statusObject);
+
+    for (const key of keys) {
+        const new_status = statusObject[key]
+        new_status.uuid = key;
+        result.push(new_status);
+    }
+
+    return result;
 }
 
 customElements.define("status-display", StatusDisplay);
