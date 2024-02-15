00603d93269e5dd34d6a66549f2df05ccbe88f82
6Botter7 <7williamsandre7@gmail.com>
2024-02-15 11:34:40 +0200

    Cleaned up Asset Tree extension

--- extensions/asset_tree.py
+++ extensions/asset_tree.py
@@ -13,57 +13,50 @@ class AssetTreeModule:
         api.logger.info(f'perform locate_element: {step["args"]["query"]}')
 
         args = step["args"]
-
-        query = args.get("query")
         element = await api.call("selenium", "get", args, ctx, process, item)
-
         driver = api.get_variable("driver")
 
-        if driver is not None:
+        parameters = {
+            "id": args["element_id"],
+            "type": args["element_type"]
+        }
+        # Execute the JavaScript function on the element
+        driver.execute_script("arguments[0].expandToEntity.apply(arguments[0], arguments[1])", element, [parameters])
 
-            parameters = {
-                "id": args["element_id"],
-                "type": args["element_type"]
+        data_id_value = f"{args['element_type']}_{args['element_id']}"
+
+        # Locate the new element by its data-id attribute
+        new_element = await find_tree_element(driver, data_id_value)
+        element_info = None
+
+        if new_element:
+            element_info = {
+                "tag_name": new_element.tag_name,
+                "text": new_element.text,
+                "location": new_element.location,
+                "id": new_element.get_attribute("id"),
+                "data_id": new_element.get_attribute("data-id")
             }
-            # Execute the JavaScript function on the element
-            driver.execute_script("arguments[0].expandToEntity.apply(arguments[0], arguments[1])", element, [parameters])
 
-            data_id_value = f"{args['element_type']}_{args['element_id']}"
-
-            # Locate the new element by its data-id attribute
-            new_element = await AssetTreeModule.find_tree_element(driver, data_id_value)
-
-            element_info = None
-
-            if new_element:
-                element_info = {
-                    "tag_name": new_element.tag_name,
-                    "text": new_element.text,
-                    "location": new_element.location,
-                    "id": new_element.get_attribute("id"),
-                    "data_id": new_element.get_attribute("data-id")
-                }
-
-                # select the new element
-                new_element.click()
-            else:
-                print("New element not found.")
+            # select the new element
+            new_element.click()
+        else:
+            print("New element not found.")
 
         # Save the element information to a variable for next_step to use
-        if process is not None:
-            set_property_on_path(process, "parameters.found_element", element_info["data_id"])
+        set_property_on_path(process, "parameters.found_element", element_info["data_id"])
 
-    @staticmethod
-    async def find_tree_element(driver, data_id_value, timeout=30):
-        """
-        Find element by its data-id attribute value.
-        """
-        try:
-            element = await get_element(driver, f"[data-id='{data_id_value}']", timeout)
-            return element
-        except TimeoutException:
-            print(f"Element with data-id '{data_id_value}' not found within {timeout} seconds.")
-            return None
-        except NoSuchElementException:
-            print(f"Element with data-id '{data_id_value}' not found.")
-            return None
+
+async def find_tree_element(driver, data_id_value, timeout=30):
+    """
+    Find element by its data-id attribute value.
+    """
+    try:
+        element = await get_element(driver, f"[data-id='{data_id_value}']", timeout)
+        return element
+    except TimeoutException:
+        print(f"Element with data-id '{data_id_value}' not found within {timeout} seconds.")
+        return None
+    except NoSuchElementException:
+        print(f"Element with data-id '{data_id_value}' not found.")
+        return None
